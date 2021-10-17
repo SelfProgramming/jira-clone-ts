7bca1bf661c1b260842265ec8c6bc75b01f0cdcd
Dmytro Kyrii <dmytrokyrii@gmail.com>
2021-10-18 00:00:55 +0300

    cypress byHook func

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('hook', (dataHook: string) => {
+  return cy.get(`[data-hook=${dataHook}]`);
+});
